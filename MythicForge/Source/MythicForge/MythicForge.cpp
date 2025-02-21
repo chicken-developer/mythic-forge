1d9d28c4692d81502c5cedec390fe6ab1508a98b
chicken-developer <quynh.fullstackdev@gmail.com>
2025-02-22 03:32:12 +0700

    Init empty project

---
+++ MythicForge/Source/MythicForge/MythicForge.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "MythicForge.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, MythicForge, "MythicForge" );
