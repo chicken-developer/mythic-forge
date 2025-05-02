9cf3ca9f3d38adc5a120f3a2301e4430e302fb13
chicken-developer <quynh.fullstackdev@gmail.com>
2025-05-03 00:33:30 +0700

    Add game cpp classes

---
+++ MythicForge/Source/MythicForge/Public/MythicForge/MFRPGGameMode.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Base/MFBaseGameMode.h"
+#include "MFRPGGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MYTHICFORGE_API AMFRPGGameMode : public AMFBaseGameMode
+{
+	GENERATED_BODY()
+	
+};
